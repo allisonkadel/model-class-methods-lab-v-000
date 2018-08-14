26b3c71c27ca49764cd4eac50c4161bc364d7b2f
allisonkadel <owner@allisonkadel.com>
2018-08-14 08:21:47 -0700

    come back to remaining methods

--- app/models/captain.rb
+++ app/models/captain.rb
@@ -1,3 +1,22 @@
 class Captain < ActiveRecord::Base
   has_many :boats
+
+  def self.catamaran_operators
+    self.includes(:boats => :classifications).where(:classifications => 
+      {:name => :Catamaran}).uniq
+  end
+
+  def self.sailors
+    self.includes(:boats => :classifications).where(:classifications =>
+      {:name => :Sailboat}).uniq
+  end
+
+  def self.talented_seafarers
+    
+  end
+
+  def self.non_sailors
+
+  end
+
 end
